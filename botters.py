b3144a4b65829dff2f2941101e4b41b7bdee82a6
Ryan <rwhermeston@uh.edu>
2018-06-19 18:06:53 -0500

    added xbox/ps4 support and help command changed

--- botters.py
+++ botters.py
@@ -13,7 +13,7 @@ key = {'TRN-Api-Key':"XXXXXXXXXXX"}
 
 client = discord.Client()
 
-commands = ['!help','!stats','!winlist','!kdlist']
+commands = ['!help','!stats','!winlist','!kdlist','!statsxb','!statsps']
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -21,13 +21,17 @@ async def on_message(message):
 
     if message.content.startswith(commands[0]):
 
-        msg = '''This bot is still under construction.\n
-        Currently pc players only\n
+        msg = '''
+
+        This bot is still under construction.\n
         Usage: !stats, !help, !kdlist and !winlist\n
-        !stats `gamertag`\n
-        !winlist `gamertag`,`gamertag`,`gamertag`\n
-        !kdlist `gamertag`,`gamertag`,`gamertag`\n
+        !stats `gamertag`   --- gives pc stats\n
+        !winlist `gamertag`,`gamertag`,`gamertag`  pc only \n
+        !kdlist `gamertag`,`gamertag`,`gamertag`  pc only\n
+        !statsxb `gamertag`  --- xbox stats\n
+        !statsps `gamertag`  --- psn stats\n
         Message @ryan.h for info
+
         '''
 
         await client.send_message(message.channel,msg)
@@ -89,19 +93,38 @@ async def on_message(message):
 
 
 
-            msg = name.center(37)+'\n'
+            msg = name+'\n'
 
-            fir,sec,thrd,frth,fith = '','','','',''
-            cols = [fir,sec,thrd,frth,fith]
+
+            cols = ['','','','','']
             cols[0] = 'Overall:'.center(0) + 'Current:'.rjust(33)
             cols[1] = str(('kd: '+overallKd).center(0)+('kd: '+str(currKd)).center(57))
-            cols[2]= str(('wins: '+overallWins).center(0)+('wins: '+str(currWins)).center(53))
-            cols[3]= str(('matches: '+overallMatchesPlayed).center(0)+('matches: '+str(currMatches)).center(40))
+
+            cols[2]= str(('wins: '+overallWins).center(0)+('wins: '+str(currWins)).center(54))
+            cols[3]= str(('matches: '+overallMatchesPlayed).center(0)+('matches: '+str(currMatches)).center(39))
             cols[4]= str(('winrate: ' +overallWinrate+'%').center(0)+('winrate: '+str(currWinRate)+'%').center(45))
             for i in range(len(cols)):
                 msg+='\n'+cols[i]
 
+            #CURRENT FORMAT
             '''
+            wsg = ''
+            coals = ['','','','','']
+            coals[0] ='Overall:','Current:'
+            coals[1]='kd: '+str(overallKd) , 'kd: '+str(currKd)
+            coals[2]='wins: '+str(overallWins),'wins: '+str(currWins)
+            coals[3]='matches:'+str(overallMatchesPlayed),'matches:'+str(currMatches)
+            coals[4]='winrate: '+str(overallWinrate)+'%','winrate: '+str(currWinRate)+'%'
+            col_width = max(len(word) for row in coals for word in row)+2 # padding
+
+            for row in coals:
+                wsg+= ("".join(word.ljust(col_width) for word in row))+'\n'
+
+            msg+=wsg
+            '''
+
+            '''
+            RETIRED
             overList = [overallKd,overallWins,overallMatchesPlayed,overallWinrate+'%',]
             currList = ['\t'+str(currKd),'\t'+str(currWins),str(currMatches),str(currWinRate)+'%',]
 
@@ -264,6 +287,237 @@ async def on_message(message):
 
     if message.content[0]=='!' and message.content.split()[0] not in commands:
         msg = 'Command not recognized, type !help to display commands'
+        await client.send_message(message.channel,msg)
+
+
+    if message.content.split()[0]==commands[4]:
+
+        name = ''
+        '''
+        for i in range(len(message.content.split())):
+            if i == 0:
+                continue
+            name+=message.content.split()[i]+" "
+        '''
+        #name should be extraced differently, above method is trash
+        #the data given is a string so we could just slice from the command length to end of string
+        name+=message.content[len(commands[4])+1:]
+        #this way of handling names is cleaner to read and elmininates an uneccesary for loop
+
+        URL = "https://api.fortnitetracker.com/v1/profile/xbox/"+name
+        try:
+            r = requests.get(url = URL,headers=key)
+            data = r.json()
+
+
+            overallKd = data['lifeTimeStats'][11]['value']
+            overallWins = data['lifeTimeStats'][8]['value']
+            overallMatchesPlayed = data['lifeTimeStats'][7]['value']
+            overallWinrate = int(overallWins)/int(overallMatchesPlayed)*100
+            overallWinrate=  str(int(overallWinrate))
+        except:
+            msg = 'Failed to gather any stats for '+name
+            await client.send_message(message.channel,msg)
+            return
+            #overall data above, current below, quick and dirty but is close to accurate
+        try:
+            currKills = int(data['stats']['curr_p9']['kills']['value'])+int(data['stats']['curr_p10']['kills']['value'])+int(data['stats']['curr_p2']['kills']['value'])
+
+
+            s1 = data['stats']['curr_p2']['matches']['value']
+            s2 = data['stats']['curr_p9']['matches']['value']
+            s3 = data['stats']['curr_p10']['matches']['value']
+            currMatches = int(s1)+int(s2)+int(s3)
+
+            currWins = str(int(data['stats']['curr_p2']['top1']['value'])+int(data['stats']['curr_p9']['top1']['value'])+int(data['stats']['curr_p10']['top1']['value']))
+
+            currWinRate =int((int(currWins)/int(currMatches)) * 100)
+
+            currKd = round(currKills/(currMatches-int(currWins)),2)
+            #vars are overallWinrate,matches,overallWins,overallKd
+            #print(str(currWinRate)+'%\n'+str(currMatches)+'\n'+str(currWins)+'\n'+str(currKd))
+        except:
+            msg = ':( failed to gather current info for '+name
+            msg+=' only overall stats will be displayed.\n'
+            msg+="kd: "+overallKd+'\n'+"wins: "+overallWins+'\n'+"matches: "+overallMatchesPlayed+'\n'+"winrate: "+overallWinrate+"%"
+            await client.send_message(message.channel,msg)
+            return
+
+        try:
+
+
+
+
+            msg = name+'\n'
+
+
+            cols = ['','','','','']
+            cols[0] = 'Overall:'.center(0) + 'Current:'.rjust(33)
+            cols[1] = str(('kd: '+overallKd).center(0)+('kd: '+str(currKd)).center(57))
+
+            cols[2]= str(('wins: '+overallWins).center(0)+('wins: '+str(currWins)).center(54))
+            cols[3]= str(('matches: '+overallMatchesPlayed).center(0)+('matches: '+str(currMatches)).center(39))
+            cols[4]= str(('winrate: ' +overallWinrate+'%').center(0)+('winrate: '+str(currWinRate)+'%').center(45))
+            for i in range(len(cols)):
+                msg+='\n'+cols[i]
+
+            #CURRENT FORMAT
+            '''
+            wsg = ''
+            coals = ['','','','','']
+            coals[0] ='Overall:','Current:'
+            coals[1]='kd: '+str(overallKd) , 'kd: '+str(currKd)
+            coals[2]='wins: '+str(overallWins),'wins: '+str(currWins)
+            coals[3]='matches:'+str(overallMatchesPlayed),'matches:'+str(currMatches)
+            coals[4]='winrate: '+str(overallWinrate)+'%','winrate: '+str(currWinRate)+'%'
+            col_width = max(len(word) for row in coals for word in row)+2 # padding
+
+            for row in coals:
+                wsg+= ("".join(word.ljust(col_width) for word in row))+'\n'
+
+            msg+=wsg
+            '''
+
+            '''
+            RETIRED
+            overList = [overallKd,overallWins,overallMatchesPlayed,overallWinrate+'%',]
+            currList = ['\t'+str(currKd),'\t'+str(currWins),str(currMatches),str(currWinRate)+'%',]
+
+            descList = ['kd:  ','wins:','matches:','winrate:']
+            msg+='Overall:\t\t\t' + 'Current:'+'\n'
+            for e,i,j in zip(descList,overList,currList):
+                msg +=('{} {} \t\t\t{}'.format(e,i,j))+'\n'
+            '''
+            currKd = float(currKd)
+
+            if (currKd) <= 1.0:
+                msg+= '\nstatus: no skin'
+            elif (currKd) >= 1.0 and currKd<=2.0:
+                msg+= '\nstatus: Rust Lord'
+            elif (currKd) >= 2.0 and currKd <=3.0:
+                msg+= '\nstatus: Dark Knight'
+            elif (currKd) >= 3.0:
+                msg+='\nstatus: Streamer'
+            else:
+                msg+= '\nstatus: no idea ask Ryan'
+
+
+        except:
+            msg = 'Failed during msg creation'
+        await client.send_message(message.channel,msg)
+    if message.content.split()[0]==commands[5]:
+        name = ''
+        '''
+        for i in range(len(message.content.split())):
+            if i == 0:
+                continue
+            name+=message.content.split()[i]+" "
+        '''
+        #name should be extraced differently, above method is trash
+        #the data given is a string so we could just slice from the command length to end of string
+        name+=message.content[len(commands[5])+1:]
+        #this way of handling names is cleaner to read and elmininates an uneccesary for loop
+
+        URL = "https://api.fortnitetracker.com/v1/profile/psn/"+name
+        try:
+            r = requests.get(url = URL,headers=key)
+            data = r.json()
+
+
+            overallKd = data['lifeTimeStats'][11]['value']
+            overallWins = data['lifeTimeStats'][8]['value']
+            overallMatchesPlayed = data['lifeTimeStats'][7]['value']
+            overallWinrate = int(overallWins)/int(overallMatchesPlayed)*100
+            overallWinrate=  str(int(overallWinrate))
+        except:
+            msg = 'Failed to gather any stats for '+name
+            await client.send_message(message.channel,msg)
+            return
+            #overall data above, current below, quick and dirty but is close to accurate
+        try:
+            currKills = int(data['stats']['curr_p9']['kills']['value'])+int(data['stats']['curr_p10']['kills']['value'])+int(data['stats']['curr_p2']['kills']['value'])
+
+
+            s1 = data['stats']['curr_p2']['matches']['value']
+            s2 = data['stats']['curr_p9']['matches']['value']
+            s3 = data['stats']['curr_p10']['matches']['value']
+            currMatches = int(s1)+int(s2)+int(s3)
+
+            currWins = str(int(data['stats']['curr_p2']['top1']['value'])+int(data['stats']['curr_p9']['top1']['value'])+int(data['stats']['curr_p10']['top1']['value']))
+
+            currWinRate =int((int(currWins)/int(currMatches)) * 100)
+
+            currKd = round(currKills/(currMatches-int(currWins)),2)
+            #vars are overallWinrate,matches,overallWins,overallKd
+            #print(str(currWinRate)+'%\n'+str(currMatches)+'\n'+str(currWins)+'\n'+str(currKd))
+        except:
+            msg = ':( failed to gather current info for '+name
+            msg+=' only overall stats will be displayed.\n'
+            msg+="kd: "+overallKd+'\n'+"wins: "+overallWins+'\n'+"matches: "+overallMatchesPlayed+'\n'+"winrate: "+overallWinrate+"%"
+            await client.send_message(message.channel,msg)
+            return
+
+        try:
+
+
+
+
+            msg = name+'\n'
+
+
+            cols = ['','','','','']
+            cols[0] = 'Overall:'.center(0) + 'Current:'.rjust(33)
+            cols[1] = str(('kd: '+overallKd).center(0)+('kd: '+str(currKd)).center(57))
+
+            cols[2]= str(('wins: '+overallWins).center(0)+('wins: '+str(currWins)).center(54))
+            cols[3]= str(('matches: '+overallMatchesPlayed).center(0)+('matches: '+str(currMatches)).center(39))
+            cols[4]= str(('winrate: ' +overallWinrate+'%').center(0)+('winrate: '+str(currWinRate)+'%').center(45))
+            for i in range(len(cols)):
+                msg+='\n'+cols[i]
+
+            #CURRENT FORMAT
+            '''
+            wsg = ''
+            coals = ['','','','','']
+            coals[0] ='Overall:','Current:'
+            coals[1]='kd: '+str(overallKd) , 'kd: '+str(currKd)
+            coals[2]='wins: '+str(overallWins),'wins: '+str(currWins)
+            coals[3]='matches:'+str(overallMatchesPlayed),'matches:'+str(currMatches)
+            coals[4]='winrate: '+str(overallWinrate)+'%','winrate: '+str(currWinRate)+'%'
+            col_width = max(len(word) for row in coals for word in row)+2 # padding
+
+            for row in coals:
+                wsg+= ("".join(word.ljust(col_width) for word in row))+'\n'
+
+            msg+=wsg
+            '''
+
+            '''
+            RETIRED
+            overList = [overallKd,overallWins,overallMatchesPlayed,overallWinrate+'%',]
+            currList = ['\t'+str(currKd),'\t'+str(currWins),str(currMatches),str(currWinRate)+'%',]
+
+            descList = ['kd:  ','wins:','matches:','winrate:']
+            msg+='Overall:\t\t\t' + 'Current:'+'\n'
+            for e,i,j in zip(descList,overList,currList):
+                msg +=('{} {} \t\t\t{}'.format(e,i,j))+'\n'
+            '''
+            currKd = float(currKd)
+
+            if (currKd) <= 1.0:
+                msg+= '\nstatus: no skin'
+            elif (currKd) >= 1.0 and currKd<=2.0:
+                msg+= '\nstatus: Rust Lord'
+            elif (currKd) >= 2.0 and currKd <=3.0:
+                msg+= '\nstatus: Dark Knight'
+            elif (currKd) >= 3.0:
+                msg+='\nstatus: Streamer'
+            else:
+                msg+= '\nstatus: no idea ask Ryan'
+
+
+        except:
+            msg = 'Failed during msg creation'
         await client.send_message(message.channel,msg)
 
 @client.event
